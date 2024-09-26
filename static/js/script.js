f1090418e14653358a6f04ef7099ae0e2662cc6c
Lucy Dang <thanhlieuco7@gmail.com>
2024-09-26 13:17:38 +0800

    Move Javascript to seperate file script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,17 @@
-// Generate random quote by reloading page once button clicked
-document.getElementById("nextButton").onclick = function() {
-    // Reload the page
-    location.reload();
-};
+document.getElementById("nextButton").addEventListener("click", function() {
+    fetch("/")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Cannot fetch new image quote")
+            }
+
+            // Update image source to refresh it
+            const image = document.getElementById("quoteImage");
+
+            // Add timestamp to avoid caching
+            image.src = "static/images/quote_image.jpg?" + new Date().getTime();
+        })
+        .catch(error => {
+            console.error("There was a problem with generating new quote image", error);
+        });
+});
