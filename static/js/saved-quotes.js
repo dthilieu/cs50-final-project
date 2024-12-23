881fc6281e1ec6101c1a73ba2615ad8845b5e28a
Lucy Dang <thanhlieuco7@gmail.com>
2024-12-23 16:55:26 +0800

    Add CSS for heart icon(red and white state), add JavaScript to toggle the heart icon and send a request to remove quote

---
+++ static/js/saved-quotes.js
@@ -0,0 +1,31 @@
+// Event listener for heart button click
+document.querySelectorAll(".heart-button").forEach(button => {
+    button.addEventListener("click", function() {
+        // Get the image_id from data attribute
+        const image_id = button.getAttribute("image_id");
+
+        // Toggle the "clicked" class to change heart icon apperance
+        button.classList.toggle("clicked");
+
+        // Send an AJAX request to the backend to remove quote from saved quotes
+        fetch("/remove-saved-quote", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            // Send the quote image_id
+            body: JSON.stringify({ image_id: image_id })
+        })
+            .then((response) =>response.json())
+            .then((data) => {
+                if (data.message) {
+                    console.log(data.message);
+                } else {
+                    console.error(data.error || "Error remove quote");
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    });
+});
