84dc0d69ec3932d635fa7ab391ce85f111b71e04
Lucy Dang <thanhlieuco7@gmail.com>
2024-12-26 10:13:07 +0800

    Add login.html, adjust class name and css file name for authentication forms

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def login():
         
         # Query database for username
         rows = db.execute(
-            "SELECT * FROM users WHERE usersname = ?", request.form.get("username")
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
             )
         
         # Ensure username exists and password is correct
